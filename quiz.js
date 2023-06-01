e1a57cc1b5025ac622ed24da4730ca657a6835ae
Siran-Li <70839645+Siran-Li@users.noreply.github.com>
2023-06-01 12:00:38 +0200

    push all

--- quiz.js
+++ quiz.js
@@ -1,54 +1,26 @@
 // var json = require('data/human_eval.json');
 import json from './data/human_eval.json' assert {'type': 'json'};
 console.log(json);
-let questions = json['5'];
-// async function loadConfigData(){
-//     const response = await fetch('./data/human_eval.json');
-//     const data = await response.json();
-//     console.log(data);
-// }
-// loadConfigData();
-// let questions = json['5'];
-// let questions = [
-//     {
-//         id: 1,
-//         triplet: "[grass, at location, tree] <br> <br>",
-//         answer: "Green grass",
-//         question: "A: What is surrounding the field and surrounding it?  <br><br>\
-//                    B: What is surrounding the field and near a track field?",
-//         gt_answer:"Question A",
-//         options: [
-//             "Question A",
-//             "Question B",
-//             "Same",
-//             "None of these"
-//         ]   
-//     },
-//     {
-//         id: 2,
-//         triplet: "[grass, at location, tree] <br> <br>",
-//         answer: "Green grass",
-//         question: "<br> A: What is surrounding the field and surrounding it?  <br>\
-//                    <br> B: What is surrounding the field and near a track field?",
-//         gt_answer:"Question A",
-//         options: [
-//             "Question A",
-//             "Question B",
-//             "Same",
-//             "None of these"
-//         ]   
-//       },
+let group = sessionStorage.getItem("group");
+let questions = json[group];
+console.log(group)
 
-// ];
 
 let question_count = 0;
-let points = 0;
 var selection = {};
-let options = ['Question A', 'Question B', 'Same']
+let options = ['Question A', 'Question B', 'Same'];
 
 window.onload = function(){
     show(question_count);
+    define();
+    console.log(selection)
 };
+
+function define(){
+    for(let i=0; i < questions.length; i++){
+        selection[questions[i].id] = "None"
+    }
+}
 
 function process(count){
     let progressbar = document.getElementById("progress");
@@ -60,14 +32,19 @@ function process(count){
     `;
 }
 
+function show_image(count){
+    let image_place = document.getElementById("image");
+    image_place.src = questions[count].image;
+    image_place.title = questions[count].caption;
+}
+
+
 function show(count){
     process(count);
+    show_image(count);
     let question = document.getElementById("questions");
-    let[first, second, third] = questions[count].options;
+    let[first, second, third] = options;
 
-    // <h2>Q${count + 1}. ${questions[count].question}</h2>
-    // triplet.innerHTML = `<h3> Knowledge Triplet: ${questions[count].triplet} <br> <br> Answer:${questions[count].answer}</h3>`;
-    // triplet.innerHTML = `<h3> Knowledge Triplet </h3>`;
     question.innerHTML = `
     <ul class="quiz_header"> 
     <li class="quiz_user">
@@ -102,24 +79,36 @@ function toggleActive(count){
 
 window.next = function() {
 
-    if(question_count == questions.length -1){
-        location.href = "final.html";
-        submitall();
+    if(selection[questions[question_count].id]!='None'){
+        if(question_count == questions.length -1){
+            location.href = "final.html";
+            submitall();
+        }
+        question_count++;
     }
-    console.log(question_count);
-
-
-let user_answer = document.querySelector("li.option.active").innerHTML;
-
-
-if(user_answer == questions[question_count].answer){
-    points += 10;
-    sessionStorage.setItem("points",50);
+    show(question_count);
+    console.log(selection);
 }
-console.log(points);
 
-question_count++;
-show(question_count);
+window.back = function() {
+    if(question_count>0){
+        question_count--;
+        deActive(question_count);
+        show(question_count);
+        console.log(selection);
+    }
+
+}
+
+function deActive(count){
+    let option = document.querySelectorAll("li.option");
+    for(let i=0; i < option.length; i++){
+        if(option[i].classList.contains("active")){
+            option[i].classList.remove("active");
+        }
+        selection[questions[count].id] = 'None';
+
+    }
 }
 
 
@@ -128,4 +117,20 @@ function submitall(){
     sessionStorage.setItem('select_0',selection[questions[0].id]);
     sessionStorage.setItem('id_1', questions[1].id);
     sessionStorage.setItem('select_1',selection[questions[1].id]);
+    sessionStorage.setItem('id_2', questions[2].id);
+    sessionStorage.setItem('select_2',selection[questions[2].id]);
+    sessionStorage.setItem('id_3', questions[3].id);
+    sessionStorage.setItem('select_3',selection[questions[3].id]);
+    sessionStorage.setItem('id_4', questions[4].id);
+    sessionStorage.setItem('select_4',selection[questions[4].id]);
+    sessionStorage.setItem('id_5', questions[5].id);
+    sessionStorage.setItem('select_5',selection[questions[5].id]);
+    sessionStorage.setItem('id_6', questions[6].id);
+    sessionStorage.setItem('select_6',selection[questions[6].id]);
+    sessionStorage.setItem('id_7', questions[7].id);
+    sessionStorage.setItem('select_7',selection[questions[7].id]);
+    sessionStorage.setItem('id_8', questions[8].id);
+    sessionStorage.setItem('select_8',selection[questions[8].id]);
+    sessionStorage.setItem('id_9', questions[9].id);
+    sessionStorage.setItem('select_9',selection[questions[9].id]);
 }
